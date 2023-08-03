caad7d3b258be07d76dbe99c9a8e3b78241bd3e3
Bilal Ahmad <bilalahmad.khan@hotmail.com>
2023-08-03 13:50:51 +0500

    Added env sample file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require("express")
+const express = require("express");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const sendMail = require("./controllers/sendMail")
 
